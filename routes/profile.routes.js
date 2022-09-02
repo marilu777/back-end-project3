c19d4432d98b1a24d2b9ed198dffb25efbea1cd6
marilu777 <maria.valentes@hotmail.com>
2022-09-02 10:48:09 +0100

    last

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -7,17 +7,28 @@ const User = require("../models/User.model");
 const Vote = require("../models/Vote.model");
 const Comment = require("../models/Comment.model");
 
-router.put('/user/:userId', (req, res, next) => {
+router.put('/user/:userId/edit', async (req, res, next) => {
   const {userId} = req.params;
-  const {username, email, contact, imgURL} = req.body;
+  const {username, email, contact} = req.body;
 
-  User.findByIdAndUpdate(userId, {username, email, contact, imgURL}, {new: true})
-  .then((user) => res.status(201).json(user))
+  User.findByIdAndUpdate(userId, {username: username, email:email, contact:contact}, {new: true})
+  .then((response) => res.json(response))
   .catch((err) => res.json(err));
   });
 
+  router.get('/user/:userId', async (req, res, next) => {
+    const {userId} = req.params;
+
+  
+    User.findById(userId)
+    .then((response) => res.json(response))
+    .catch((err) => res.json(err));
+    });
+
+
 router.delete('/user/:userId', (req, res, next) => {
   const {userId} = req.params;
+  const { username, email, contact, city, nif} = req.body;
   User.findByIdAndRemove(userId)
   .then((response) => {
     console.log(response);
